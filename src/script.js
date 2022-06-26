fbebda27f2cf27bb58f8e75ef9b88565f1686060
cs <cspencernd@gmail.com>
2022-06-26 11:43:59 -0400

    add responsive nav

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 const btn = document.getElementById("menu-btn")
-const nav = document.getElementById("menu")
+const nav = document.getElementById("nav-menu")
 
 btn.addEventListener("click", () => {
     btn.classList.toggle("open")
-    nav.classList.toggle("flex")
-    nav.classList.toggle("hidden")
+    nav.classList.toggle("translate-x-0")
+    nav.classList.toggle("translate-x-full")
 })
