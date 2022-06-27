f4ec43114c3e6ca0a3087f3befa04677a69cb44a
cs <cspencernd@gmail.com>
2022-06-27 08:03:18 -0400

    add login card

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
-const btn = document.getElementById("menu-btn")
-const nav = document.getElementById("nav-menu")
+const btn = document.getElementById("menuButton")
+const nav = document.getElementById("navMenu")
 
 btn.addEventListener("click", () => {
     btn.classList.toggle("open")
     nav.classList.toggle("translate-x-0")
-    nav.classList.toggle("translate-x-full")
+    // nav.classList.toggle("translate-x-full")
 })
