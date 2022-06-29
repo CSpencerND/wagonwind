36a40ab701967d023fecb8ffac62331f7e9d4102
cs <cspencernd@gmail.com>
2022-06-29 06:11:36 -0400

    login modal with blurred backdrop

--- src/script.js
+++ src/script.js
@@ -1,8 +1,38 @@
-const btn = document.getElementById("menuButton")
+// {{{ hamburger menu 
+const button = document.getElementById("menuButton")
 const nav = document.getElementById("navMenu")
 
-btn.addEventListener("click", () => {
-    btn.classList.toggle("open")
+button.addEventListener("click", () => {
+    button.classList.toggle("open")
     nav.classList.toggle("translate-x-0")
     // nav.classList.toggle("translate-x-full")
 })
+// }}}
+
+// {{{ login modal 
+const modal = document.getElementById("modal")
+const openModal = document.getElementById("loginButton")
+
+openModal.addEventListener("click", () => {
+    modal.showModal()
+})
+
+modal.addEventListener("click", (e) => {
+    const rect = modal.getBoundingClientRect()
+
+    if (
+        e.clientY < rect.top ||
+        e.clientY > rect.bottom ||
+        e.clientX < rect.left ||
+        e.clientX > rect.right
+    ) {
+        modal.close()
+    }
+})
+
+document.onkeyup = (event) => {
+    if (event.key === "Escape") {
+        modal.close()
+    }
+}
+// }}}
