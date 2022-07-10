5d0a42383905eaea07b62c0c47cbf81b58b073d4
cs <cspencernd@gmail.com>
2022-07-09 23:53:01 -0400

    sub heroicons for fa; refactor nav-menu; refactor dark toggle;

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,40 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     darkMode: "class",
     theme: {
+        screens: {
+            // max-width breakpoints
+            "max-2xl": { max: "1535px" },
+            // => @media (max-width: 1535px) { ... }
+
+            "max-xl": { max: "1279px" },
+            // => @media (max-width: 1279px) { ... }
+
+            "max-lg": { max: "1023px" },
+            // => @media (max-width: 1023px) { ... }
+
+            "max-md": { max: "767px" },
+            // => @media (max-width: 767px) { ... }
+
+            "max-sm": { max: "639px" },
+            // => @media (max-width: 639px) { ... }
+
+
+            // min-width breakpoints
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+        },
         extend: {
             colors: {
                 "drac-bg": "hsl(231 15% 18% / 1)",
@@ -15,10 +49,8 @@ module.exports = {
                 "drac-yellow": "hsl(65 92% 76% / 1)",
                 "drac-orange": "hsl(31 100% 71% / 1)",
                 "drac-red": "hsl(0 100% 76% / 1)",
-            }
+            },
         },
     },
-    plugins: [
-        require("@tailwindcss/forms")
-    ],
+    plugins: [require("@tailwindcss/forms")],
 }
